d46a0e32cd55a1186d6d70125cfd7b23125b957b
+ <+>
2022-06-07 06:21:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
+return (feet>2500)? 'No can do.': (feet <=400)? 'This one is on me!': 'I will glady take your thirty bucks.;'  
 }
 
 function ternaryCheckCity(){
-  // Write your code here!
-}
+  return (city === 'NYC')? 'ok,sounds good.' :'No global.';
+}  
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+  switch(tip) {
+    case 'generous';
+     return 'Thank you so much.';
+    case 'not as generous';
+     return 'Thank you.';
+    case 'thanks for everything'
+     return 'Bye';
+  } 
 }
