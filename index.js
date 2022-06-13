df33643db5aa7ba06e0316ae19095079daada2db
+ <+>
2022-06-13 14:57:23 +0300

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
-function scuberGreetingForFeet(){
-return (feet>2500)? 'No can do.': (feet <=400)? 'This one is on me!': 'I will glady take your thirty bucks.;'  
+function scuberGreetingForFeet(someValue){
+  if(someValue <= 400){
+    return 'This one is on me!'}
+  else if(someValue > 2000 && someValue <= 2500){
+    return 'I will gladly take your thirty bucks.'}
+  else if(someValue > 2500){
+    return 'No can do.'}
 }
 
-function ternaryCheckCity(){
-  return (city === 'NYC')? 'ok,sounds good.' :'No global.';
-}  
+function ternaryCheckCity(someValue){
+  return someValue === 'NYC'?'Ok, sounds good.' :'No go.'
+}
 
-function switchOnCharmFromTip(){
-  switch(tip) {
-    case 'generous';
-     return 'Thank you so much.';
-    case 'not as generous';
-     return 'Thank you.';
-    case 'thanks for everything'
-     return 'Bye';
-  } 
+function switchOnCharmFromTip(someValue){
+  switch(someValue){
+    case 'generous':
+      return 'Thank you so much.'
+    case 'not as generous':
+      return 'Thank you.'
+    default:
+      return 'Bye.'
+  }
 }
